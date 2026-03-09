54958f403baa7f76aba72c3da10cc4519ec1614d
minpeter <minpeter@friendli.ai>
2026-03-09 20:56:11 +0900

    refactor(headless): extract signal handler boilerplate

--- packages/headless/src/index.ts
+++ packages/headless/src/index.ts
@@ -1,4 +1,5 @@
 // biome-ignore lint/performance/noBarrelFile: package public entrypoint must aggregate exports.
 export * from "./emit";
+export * from "./signals";
 export * from "./stream-processor";
 export * from "./types";
